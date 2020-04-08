619a7645bcea20c05d76f4be039c7a61cfd0e54b
Kaden <kdex95@yahoo.com>
2020-04-07 19:16:07 -0600

    I'm trying to get the architecture of my files in a good state

--- ConditionPane.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.layout.StackPane;
-
-public class ConditionPane extends StackPane
-{
-    ConditionPane()
-    {
-        this.getChildren().add(new Conditions());
-    }
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# SOTM
-GUI program for aid playing Sentinels of the Multiverse
-This is some testing
-
